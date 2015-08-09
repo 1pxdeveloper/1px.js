c93dd0e205ebb17f63df4034225cfc15e0e0f194
유용태 <developer@1px.kr>
2015-08-09 12:02:58 +0900

    다시 시작한다. 업데이트 고고!

--- old/1px.js
+++ old/1px.js
@@ -5,7 +5,7 @@ document.writeln('<script src="1px.dom.js"></script>');
 document.writeln('<script src="1px.compile.js"></script>');
 document.writeln('<script src="1px.directives.js"></script>');
 
-document.writeln('<script src="lib/1px.cookie.js"></script>');
+document.writeln('<script src="../lib/1px.cookie.js"></script>');
 //document.writeln('<script src="lib/1px.promise.js"></script>');
 //document.writeln('<script src="lib/1px.http.js"></script>');
 //document.writeln('<script src="lib/1px.rest.js"></script>');
