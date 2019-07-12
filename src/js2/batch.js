0d7d812f578efe5cf5b8df6425d2596c92084c33
유용태 <>
2019-07-12 11:42:50 +0900

    최신 소스 업데이트

--- src/js2/batch.js
+++ src/js2/batch.js
@@ -1,19 +1,19 @@
 (function() {
 	"use strict";
 	
-	
 	/// @TODO:: 여기는 엔진 코드와 실제 사용 코드간의 인터페이스를 억지로라도 맞춰주는 코드
 	/// @TODO: 아직 인터페이스에 대한 확신이 안 듬..
 	
 	
+	const {$module} = require("./1px.module");
 	const {Observable} = require("./observable");
-	const {$module} = require("./module");
 	const {WebComponent} = require("./component");
 	
 	window.Observable = Observable;
 	window.$module = $module;
 	
-
+	$module.value("Observable", Observable);
+	
 	$module.value("WebComponent", WebComponent);
 	
 	$module.component = function(name, block) {
@@ -21,19 +21,16 @@
 			throw TypeError("name is String..")
 		}
 		
-		name = name.toLowerCase();
-		WebComponent.template.tagName = name;
-		
+		block.tagName = name.toLowerCase();
+
 		$module.require(block, component => {
 			window.customElements.define(name, component);
+			delete WebComponent.template.tagName;
 		});
-	}
+	};
 	
-
+	
 	/// 이건 너무 별론데...
-	
 	const {JSContext} = require("./parse");
 	$module.value("JSContext", JSContext);
-	
-	
 })();
--- src/js2/component.js
+++ src/js2/component.js
@@ -1,10 +1,10 @@
 (function() {
 	"use strict";
-	
+
 	const {JSContext, $compile} = require("./compile");
-	
+
 	const $$templates = Object.create(null);
-	
+
 	class WebComponent extends HTMLElement {
 
 		/// @TODO: template 연동은 임시..
@@ -12,9 +12,11 @@
 			let html = String.raw(...arguments);
 			let wrap = document.createElement("body");
 			wrap.innerHTML = html;
+			
 			$$templates[WebComponent.template.tagName] = wrap.getElementsByTagName("template")[0];
+			console.log(WebComponent.template.tagName, $$templates[WebComponent.template.tagName])
 		}
-		
+
 		static templateSelector(selector) {
 			console.log(WebComponent.template.tagName, document.querySelector(selector));
 			$$templates[WebComponent.template.tagName] = document.querySelector(selector);
@@ -24,27 +26,21 @@
 			super();
 			this.created();
 		}
-		
+
 		connectedCallback() {
 			this.$$originalContent = document.createDocumentFragment();
-			while(this.childNodes.length) {
+			while (this.childNodes.length) {
 				this.$$originalContent.appendChild(this.childNodes[0]);
 			}
-			
+
 			let context = JSContext.connect(this);
-			this.init(context);
-			
-			
 			let name = this.tagName.toLowerCase();
 			let template = $$templates[name];
-			
-			console.log(template);
-			
-			
+
 			if (template) {
 				template = template.cloneNode(true);
-				$compile(template, context);
-				
+				$compile(template, context, this);
+
 				// /// Attach Shady DOM!!
 				// let contents = DocumentFragment.from(this.childNodes);
 				//
@@ -58,31 +54,34 @@
 				// 	content.replaceWith(contents);
 				// }
 				// this.appendChild(template);
-				
-				
+
+
 				this.appendChild(template.content);
 			}
-			
+
+			this.$ = context;
+			this.init(context);
 			this.connected();
 		}
-		
+
 		disconnectedCallback() {
 			this.innerHTML = "";
 			this.appendChild(this.$$originalContent);
 			delete this.$$originalContent;
+			this.$.disconnect();
 			this.disconnected();
 		}
-		
+
 		init() {}
-		
+
 		created() {}
-		
+
 		connected() {}
-		
+
 		disconnected() {}
 	}
-	
+
 	exports.WebComponent = WebComponent;
-	
+
 })();
 
